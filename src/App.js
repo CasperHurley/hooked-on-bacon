2cab3713e2a34dabf259564b57f9f67c4bc9ccf4
CasperHurley <joseph.casper.hurley@gmail.com>
2020-01-09 17:01:00 -0500

    Sizzle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [numberOfParagraphs, updateNumberOfParagraphs] = useState(1);
+  const [bacon, setBacon] = useState([]);
+
+  useEffect(() => {
+    axios(`https://baconipsum.com/api/?type=all-meat&paras=${numberOfParagraphs}`).then(response => {
+      setBacon(response.data);
+    })
+  }, [numberOfParagraphs]);
+
+  const handleChange = (e) => {
+    updateNumberOfParagraphs(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="updateNumOfPar">How much bacon would you like today?</label>
+      <br />
+      <input id="updateNumOfPar" style={{width: '4em', padding: '5px'}} type="number" value={numberOfParagraphs} min="0" onChange={handleChange} />
+      {numberOfParagraphs >= 10 && <h3>That's a lot of bacon. Ron Swanson would approve. Merica.</h3>}
+      {
+        bacon.map((paragraph, index) => {
+          return (
+            <p key={index}>{paragraph}</p>
+          )
+        })
+      }
     </div>
   );
 }
